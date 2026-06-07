22dd19840597f9f841b5d2fe394fb3262b83906a
nameidkman <Saianshbhatia77@gmail.com>
2026-06-07 00:45:33 -0400

    hopefully this is working

---
+++ test.py
@@ -0,0 +1,8 @@
+
+import socket
+
+addr = ("", 8080)  # all interfaces, port 8080
+if socket.has_dualstack_ipv6():
+    s = socket.create_server(addr, family=socket.AF_INET6, dualstack_ipv6=True)
+else:
+    s = socket.create_server(addr)
